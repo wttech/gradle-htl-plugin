f39de513c7d8bc4d335b7dab1f5ea80067e80e54
mierzwid <mierzwid@gmail.com>
2019-02-06 12:02:37 +0100

    invoking real logic

--- src/main/kotlin/com/cognifide/gradle/htl/tasks/Htl.kt
+++ src/main/kotlin/com/cognifide/gradle/htl/tasks/Htl.kt
@@ -1,9 +1,11 @@
 package com.cognifide.gradle.htl.tasks
 
+import com.cognifide.gradle.htl.HtlCompiler
 import com.cognifide.gradle.htl.HtlExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.tasks.*
+import java.io.File
 import javax.inject.Inject
 
 open class Htl @Inject constructor(project: Project) : DefaultTask() {
@@ -20,14 +22,21 @@ open class Htl @Inject constructor(project: Project) : DefaultTask() {
 
     @SkipWhenEmpty
     @InputFiles
-    val htlFiles = project.fileTree(options.directory).matching { it.include(extensions) }
+    val htlFiles = project.fileTree(options.directory).matching { it.include(extensions) }.toList()
 
 
     @TaskAction
     fun htl() {
         project.logger.lifecycle("Welcome to HTL validation!")
-        htlFiles.forEach {
-            project.logger.lifecycle("${it.path}")
+        val results = HtlCompiler(File(options.directory)).compileHTLScripts(htlFiles)
+        results.filterValues { it.errors.isNotEmpty() || it.warnings.isNotEmpty() }.forEach { file, result ->
+            project.logger.lifecycle("${file.path}:")
+            result.errors.forEach {
+                project.logger.lifecycle("ERROR: ${it.message}")
+            }
+            result.warnings.forEach {
+                project.logger.lifecycle("WARNING: ${it.message}")
+            }
         }
     }
 
