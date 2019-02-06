76a0a75353dbd3c00122ac5d4cc24522d1400a0a
piotr.wilczynski <piotr.wilczynski@cognifide.com>
2019-02-06 11:31:16 +0100

    Implemented HtlCompiler

--- src/main/kotlin/com/cognifide/gradle/htl/HtlCompiler.kt
+++ src/main/kotlin/com/cognifide/gradle/htl/HtlCompiler.kt
@@ -2,9 +2,21 @@ package com.cognifide.gradle.htl
 
 import org.apache.sling.scripting.sightly.compiler.CompilationResult
 import java.io.File
+import java.util.LinkedHashMap
+import org.apache.sling.scripting.sightly.compiler.SightlyCompiler
 
-open class HtlCompiler {
-    fun compileHTLScripts(scripts: List<File>) : Map<File, CompilationResult>? {
-        return mapOf()
+open class HtlCompiler
+constructor(private val sourceDirectory: File) {
+
+    private val compiler = SightlyCompiler()
+
+    fun compileHTLScripts(scripts: List<File>) : Map<File, CompilationResult> {
+        val compilationResult = LinkedHashMap<File, CompilationResult>(scripts.size)
+        for (script: File in scripts) {
+            val scriptCompilationUnit = ScriptCompilationUnit(sourceDirectory, script)
+            compilationResult[script] = compiler.compile(scriptCompilationUnit)
+            scriptCompilationUnit.dispose()
+        }
+        return compilationResult
     }
 }
